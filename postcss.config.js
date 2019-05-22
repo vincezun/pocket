7b90a507023c6a21818574b92afd6565fb03e024
vinzun <vncnzo@yahoo.com>
2019-05-23 05:41:37 +0800

    Changed autoprefixer options

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,7 @@ module.exports = ctx => {
   const plugins = {
     autoprefixer: {
       ...ctx.options.autoprefixer,
-      browsers: ['> 0.2%', 'not ie < 11', 'last 2 versions', 'not dead'],
-      flexbox: 'no-2009',
+      browsers: ['> 1%', 'not ie <= 11', 'last 2 versions', 'not dead'],
       flexbox: true,
       grid: true
     }
