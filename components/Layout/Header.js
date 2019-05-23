27d83354da731cbb0e059310667e78a9a45605d9
vinzun <vncnzo@yahoo.com>
2019-05-24 05:25:51 +0800

    Changed scroll top distance

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 
 import Separator from '../separator/line';
-import LoginBtn from '../buttons/transparentBtn';
+import LoginBtn from '../buttons/loginBtn';
 import '../../static/styles/header.scss';
 
 class Header extends React.Component {
@@ -13,7 +13,7 @@ class Header extends React.Component {
 
     var scrollDisabled = false,
       body = document.body,
-      topDistance = 300,
+      topDistance = 400,
       scrollPosition;
 
     window.addEventListener('scroll', function() {
@@ -70,120 +70,118 @@ class Header extends React.Component {
   }
 
   render() {
-    const loginBtn = {
-      margin: '1.8em 1.6em'
-    };
-
     return (
       <section>
         <nav className='nav' id='nav'>
-          <Link href='/'>
-            <a>
-              <img
-                src='/static/images/pocket-logo.svg'
-                alt='Pocket Company logo'
-                className='companyLogo'
-              />
-            </a>
-          </Link>
+          <div className='navWrapper'>
+            <Link href='/'>
+              <a>
+                <img
+                  src='/static/images/pocket-logo.svg'
+                  alt='Pocket Company logo'
+                  className='companyLogo'
+                />
+              </a>
+            </Link>
 
-          <img
-            src='/static/images/hamburger-icon.svg'
-            alt='Hamburger icon'
-            className='hamburger'
-            id='hamburger'
-          />
+            <img
+              src='/static/images/hamburger-icon.svg'
+              alt='Hamburger icon'
+              className='hamburger'
+              id='hamburger'
+            />
 
-          <div className='overlay' id='overlay'>
-            <div className='topBar'>
-              <img
-                src='/static/images/exit-icon.svg'
-                alt='Exit icon'
-                className='exit'
-                id='exit'
-              />
+            <div className='overlay' id='overlay'>
+              <div className='topBar'>
+                <img
+                  src='/static/images/exit-icon.svg'
+                  alt='Exit icon'
+                  className='exit'
+                  id='exit'
+                />
 
-              <ul className='socialLinks'>
+                <ul className='socialLinks'>
+                  <li>
+                    <Link href='#facebook'>
+                      <a>
+                        <img
+                          src='/static/images/facebook-logo.svg'
+                          alt='Facebook logo'
+                          title='follow us on facebook'
+                        />
+                      </a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href='#twitter'>
+                      <a>
+                        <img
+                          src='/static/images/twitter-logo.svg'
+                          alt='Twitter logo'
+                          title='follow us on twitter'
+                        />
+                      </a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href='#dribbble'>
+                      <a>
+                        <img
+                          src='/static/images/dribbble-logo.svg'
+                          alt='Dribbble logo'
+                          title='follow us on dribbble'
+                        />
+                      </a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href='#behance'>
+                      <a>
+                        <img
+                          src='/static/images/behance-logo.svg'
+                          alt='Behance logo'
+                          title='follow us on behance'
+                        />
+                      </a>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+
+              <ul className='navigationLinks'>
                 <li>
-                  <Link href='#facebook'>
-                    <a>
-                      <img
-                        src='/static/images/facebook-logo.svg'
-                        alt='Facebook logo'
-                        title='follow us on facebook'
-                      />
-                    </a>
+                  <Link href='#features'>
+                    <a>FEATURES</a>
                   </Link>
                 </li>
                 <li>
-                  <Link href='#twitter'>
-                    <a>
-                      <img
-                        src='/static/images/twitter-logo.svg'
-                        alt='Twitter logo'
-                        title='follow us on twitter'
-                      />
-                    </a>
+                  <Link href='#howitworks'>
+                    <a>HOW IT WORKS</a>
                   </Link>
                 </li>
                 <li>
-                  <Link href='#dribbble'>
-                    <a>
-                      <img
-                        src='/static/images/dribbble-logo.svg'
-                        alt='Dribbble logo'
-                        title='follow us on dribbble'
-                      />
-                    </a>
+                  <Link href='#about'>
+                    <a>ABOUT</a>
                   </Link>
                 </li>
                 <li>
-                  <Link href='#behance'>
-                    <a>
-                      <img
-                        src='/static/images/behance-logo.svg'
-                        alt='Behance logo'
-                        title='follow us on behance'
-                      />
-                    </a>
+                  <Link href='#testimonial'>
+                    <a>TESTIMONIAL</a>
                   </Link>
+                </li>
+                <li>
+                  <Link href='#contact'>
+                    <a>CONTACT</a>
+                  </Link>
+                </li>
+                <li>
+                  <Separator />
+                </li>
+                <li>
+                  <LoginBtn link='#login' />
                 </li>
               </ul>
             </div>
-
-            <ul className='navigationLinks'>
-              <li>
-                <Link href='#features'>
-                  <a>FEATURES</a>
-                </Link>
-              </li>
-              <li>
-                <Link href='#howitworks'>
-                  <a>HOW IT WORKS</a>
-                </Link>
-              </li>
-              <li>
-                <Link href='#about'>
-                  <a>ABOUT</a>
-                </Link>
-              </li>
-              <li>
-                <Link href='#testimonial'>
-                  <a>TESTIMONIAL</a>
-                </Link>
-              </li>
-              <li>
-                <Link href='#contact'>
-                  <a>CONTACT</a>
-                </Link>
-              </li>
-              <li>
-                <Separator />
-              </li>
-              <li>
-                <LoginBtn name='LOGIN' link='#login' styled={loginBtn} />
-              </li>
-            </ul>
           </div>
         </nav>
       </section>
